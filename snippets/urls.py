a2ea356cc02e32b07b4b77cd5fd81d4bf80887d5
GaboritJunior <47030574+GaboritJunior@users.noreply.github.com>
2022-03-28 10:57:08 +0200

    ajout de la route de login

--- snippets/urls.py
+++ snippets/urls.py
@@ -11,5 +11,6 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    path(r'', include(router.urls))
+    path(r'', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
